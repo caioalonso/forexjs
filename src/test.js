e136dc1ceebfb0042635259e7118e3b65ced1231
Caio Alonso <caio@caioalonso.com>
2017-12-02 23:03:52 -0200

    Very ugly archaic precarious experimental ghetto trading mechanic

--- src/test.js
+++ src/test.js
@@ -4,12 +4,23 @@ const moment = require('moment')
 const Candle = require('./candle')
 const csv = require('fast-csv')
 const SMA = require('./sma')
+const EMA = require('./ema')
 
 var candles = []
-var shortSma = new SMA(10)
-var mediumSma = new SMA(50)
+var balance = {
+  'USD': 10000,
+  'EURUSD': 0
+}
+var lotSize = 100
+var shortEma = new EMA(10)
+var mediumEma = new EMA(50)
 var longSma = new SMA(100)
-var indicators = [shortSma, mediumSma, longSma]
+var indicators = [shortEma, mediumEma, longSma]
+var prevCross = [false, false]
+var curCross = [false, false]
+var position = 0
+
+var positionSide = null
 
 function runTest (file) {
   csv
@@ -17,6 +28,7 @@ function runTest (file) {
     .on('data', tick => {
       updateCandles(tick)
       runStrategy(tick)
+      console.log(balance)
     }).on('end', function(){
       console.log('done')
     })
@@ -38,7 +50,60 @@ function updateCandles (tick) {
 }
 
 function runStrategy (tick) {
-  console.log(shortSma.last())
+  if (!seenEnoughData()) {
+    return
+  }
+
+  prevCross = curCross
+  curCross = checkCrosses()
+
+  if (prevCross[0] === false && curCross[0] === true) {
+    if(positionSide === true) {
+      return
+    } else {
+      closePosition(tick[2])
+      positionSide = true
+      buy(tick[2])
+    }
+  }
+
+  if (prevCross[1] === false && curCross[1] === true) {
+    if(positionSide === false) {
+      return
+    } else {
+      closePosition(tick[2])
+      positionSide = false
+      sell(tick[2])
+    }
+  }
+}
+
+function seenEnoughData () {
+  return candles.length >= 100
+}
+
+function checkCrosses () {
+  const buycross = shortEma.last() > mediumEma.last()
+  const sellcross = shortEma.last() < mediumEma.last()
+  return [buycross, sellcross]
+}
+
+function buy (price) {
+  position = lotSize
+  balance['USD'] -= lotSize * price
+  balance['EURUSD'] += lotSize
+}
+
+function sell (price) {
+  position = lotSize
+  balance['USD'] += lotSize * price
+  balance['EURUSD'] -= lotSize
+}
+
+function closePosition (price) {
+  balance['USD'] += balance['EURUSD'] * price
+  balance['EURUSD'] = 0
+  position = 0
 }
 
 module.exports = runTest
